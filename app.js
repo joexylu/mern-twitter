eaf29b80f79464c0ce82257ae463587eae036026
joexylu <joe.luxiyang@gmail.com>
2020-03-16 20:36:00 -0700

    heroku push

--- app.js
+++ app.js
@@ -8,12 +8,21 @@ const bodyParser = require('body-parser');
 
 const passport = require('passport');
 
-const User = require('./models/User')
+const User = require('./models/User');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
